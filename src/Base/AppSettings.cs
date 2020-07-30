b12057cb525109a9586e5c7158dcb80b7663dd1c
Luke Wisniewski <luke.wisniewski@connectiverx.com>
2020-07-30 08:52:12 -0400

    - Trying to get it to build in VS2019, but no luck yet

---
+++ src/Base/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintDotNet.Base
+{
+    public static class AppSettings
+    {
+        public static string FeedbackEmail => GetSetting();
+        public static string CrashlogEmail => GetSetting();
+
+        public static string GetSetting([CallerMemberName]string name = null)
+        {
+            var value = ConfigurationManager.AppSettings[name];
+            if (string.IsNullOrEmpty(value)) throw new Exception($"Did not find {name} in the app.config file, but it is required");
+
+            return value;
+        }
+    }
+}
--- src/Resources/InvariantStrings.cs
+++ src/Resources/InvariantStrings.cs
@@ -7,7 +7,9 @@
 // .                                                                           //
 /////////////////////////////////////////////////////////////////////////////////
 
+using PaintDotNet.Base;
 using System;
+using System.Configuration;
 
 namespace PaintDotNet
 {
@@ -18,51 +20,46 @@ namespace PaintDotNet
     {
         // {0} is "All Rights Reserved"
         // Legal has advised that's the only part of this string that should be localizable.
-        public const string CopyrightFormat = 
+        public static string CopyrightFormat => 
             "Copyright © 2008 dotPDN LLC, Rick Brewster, Tom Jackson, and contributors. Portions Copyright © Microsoft Corporation. {0}";
 
-        public const string FeedbackEmail =
-              "" <-- You must specify an e-mail address for users to send feedback to.;
+        public static string FeedbackEmail => AppSettings.GetSetting();
 
-        public const string CrashlogEmail =
-              "" <-- You must specify a contact e-mail address to be placed in the crash log.;
+        public static string CrashlogEmail => AppSettings.GetSetting();
 
-        public const string WebsiteUrl =
-              "" <-- You must specify a URL for the application's website.;
+        public static string WebsiteUrl => AppSettings.GetSetting();
 
-        public const string WebsitePageHelpMenu = "/redirect/main_hm.html";
+        public static string WebsitePageHelpMenu => "/redirect/main_hm.html";
 
-        public const string ForumPageHelpPage = "/redirect/forum_hm.html";
+        public static string ForumPageHelpPage => "/redirect/forum_hm.html";
 
-        public const string PluginsPageHelpPage = "/redirect/plugins_hm.html";
+        public static string PluginsPageHelpPage => "/redirect/plugins_hm.html";
 
-        public const string TutorialsPageHelpPage = "/redirect/tutorials_hm.html";
+        public static string TutorialsPageHelpPage => "/redirect/tutorials_hm.html";
 
-        public const string DonatePageHelpMenu = "/redirect/donate_hm.html";
+        public static string DonatePageHelpMenu => "/redirect/donate_hm.html";
 
-        public const string SearchEngineHelpMenu = "/redirect/search_hm.html";
+        public static string SearchEngineHelpMenu => "/redirect/search_hm.html";
 
-        public const string DonateUrlSetup =
-            "" <-- You must specify a destination URL for the donate button in the setup wizard.;        
+        public static string DonateUrlSetup => AppSettings.GetSetting();
 
-        public const string ExpiredPage = "redirect/pdnexpired.html";
+        public static string ExpiredPage => "redirect/pdnexpired.html";
 
-        public const string EffectsSubDir = "Effects";
+        public static string EffectsSubDir => "Effects";
 
-        public const string FileTypesSubDir = "FileTypes";
+        public static string FileTypesSubDir => "FileTypes";
 
-        public const string DllExtension = ".dll";
+        public static string DllExtension => ".dll";
 
         // Fallback strings are used in case the resources file is unavailable.
-        public const string CrashLogHeaderTextFormatFallback =
+        public static string CrashLogHeaderTextFormatFallback =>
             @"This text file was created because Paint.NET crashed.
 Please e-mail this file to {0} so we can diagnose and fix the problem.
 ";
 
-        public const string StartupUnhandledErrorFormatFallback =
+        public static string StartupUnhandledErrorFormatFallback =>
             "There was an unhandled error, and Paint.NET must be closed. Refer to the file '{0}', which has been placed on your desktop, for more information.";
 
-        public const string SingleInstanceMonikerName = 
-            "" <-- You must specify a moniker name (only letters, no symbols, no spaces);
+        public static string SingleInstanceMonikerName => AppSettings.GetSetting();
     }
 }
